fee51ecf6060ac5318e861ea0d54fea48c931402
mustafaselmanonal <mustafaselman.100@gmail.com>
2024-01-03 22:49:53 +0300

    1.ders

---
+++ src/1-JSX/JSX.js
@@ -0,0 +1,12 @@
+
+
+
+const JSX = () => {
+    return(
+        <div>
+            Bu benim JSX componentim
+        </div>
+    )
+} 
+
+export default JSX
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import JSX from "./1-JSX/JSX"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <JSX/>
     </div>
   );
 }
 
 export default App;
+
+
+// function App2(){
+//   return(
+//     <div>
+//       <p>App2 içeriği</p>
+//     </div>
+//   )
+// }
+
+// export {App2}
